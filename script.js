e1ccca5274529353903d66d285ff305c7a8db4f5
Aileen Wong <aileenwong@Aileens-MacBook-Pro.local>
2018-05-03 17:12:05 -0400

    populate input boxes on luckyseat hamilton page

---
+++ script.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+  console.log('Script Running');
+
+let firstName = document.querySelector('#firstname');
+firstName.value = 'Aileen';
+
+let lastName = document.querySelector('#lastname');
+lastName.value = 'Wong';
+
+let email = document.querySelector('#email');
+email.value = 'aileen.m.wong@gmail.com';
+
+let zip = document.querySelector('#zipcode');
+zip.value = '11201';
+}
